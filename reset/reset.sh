fc389567111a6776b32a4484ad207c5d6ed6ee17
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-07-09 18:20:16 +0530

    Add files via upload

---
+++ reset/reset.sh
@@ -0,0 +1,18 @@
+re() {
+random
+echo -e -n "Enter email :\033[0m "
+read
+random
+sleep 3
+echo -e -n "Enter password\033[0m : "
+read
+sleep 9
+random
+printf "successfully changed password\n"
+
+random
+printf "presss enter\n"
+read
+xdg-open https://mbasic.facebook.com/login/identify/?ctx=recover&c=https%3A%2F%2Fmbasic.facebook.com%2F%3Fstype%3Dlo%26jlou%3DAffnOYy2VNzdPW7purenEegekBsQTZNyjnGagoWTaf7pI3rYAgN6HBc9noC5NHwMp2sxhvv67ZuycPxM3QVrNISkuVbtmdKXvmg1GVjMmUVKdg%26smuh%3D41406%26lh%3DAc-gWrweiOea7W-cLH0%26wtsid%3Drdr_0FqYSHG1r0oZbJLG7&multiple_results=0&ars=facebook_login&from_login_screen=0&lwv=100&wtsid=rdr_0jn24dmxuQSgRVR64&_rdr
+}
+re
